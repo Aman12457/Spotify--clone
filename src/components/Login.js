9e56a066872fd11329466bba46d245ab2c6e98e8
Aman Kuswah <amankushwah357@gmail.com>
2023-07-21 17:11:09 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,8 +4,7 @@ import { accessUrl } from '../spotify/spotify';
 
 
 function Login() {
-  
- return (
+  return (
     <div className="login">
       <img src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' alt=''
       />
